cc3c28402fe72119390a153d2f4b09ec533e3d36
Devil-9 <lcs201906@iiitl.ac.in>
2021-05-29 13:06:30 -0800

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter1/home_page.dart';
+import 'package:flutter1/pages/home_page.dart';
+import 'package:flutter1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-
-    String name = "Devil";
-
     return MaterialApp(
-      home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login": (context)=> LoginPage(),
+      },
+      
     );
   }
 }
