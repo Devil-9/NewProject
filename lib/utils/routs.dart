86a15efb62de9ab9206f786dedd4e66d4938b9c0
Devil-9 <johndoe@example.com>
2021-07-01 02:19:03 +0530

    New pages

--- lib/utils/routs.dart
+++ lib/utils/routs.dart
@@ -2,4 +2,5 @@ class MyRoutes {
   static String loginRoute = "/login";
   static String homeRoute = "/home";
   static String homescreenRoute = "/homescreen";
+  static String signupRoute = "/signup";
 }
